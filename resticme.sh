b77b1babb3ef8202a83a4316a1728858552fdd4a
root <root@mail.architux.com>
2018-04-02 10:51:54 +0200

    some changes

--- resticme.sh
+++ resticme.sh
@@ -1,27 +1,54 @@
 #!/bin/bash -
 
+set -x
+
 # set repo : REPO=rest:http://backup:pass@truc:8000/sauvegarde
 # set password RESTIC_PASSWORD=pass
 
+restic_credential_file=/root/.restic-credential.sh
 ops="-x"
 script_name=$(basename ${0/.sh/})
 tmp_file=$(mktemp --suffix $script_name)
-lock_file=/tmp/${script_name}.lock
+lock_file=/var/lock/.${script_name}.exclusivelock
 
-if ! [ -f $lock_file ] ; then
-	lockfile $lock_file
-else
-	echo "An instance seem will be active"  	
-	exit 2
+lock() {
+	# http://www.kfirlavi.com/blog/2012/11/06/elegant-locking-of-bash-program/
+	exec 200>$lock_file
+
+	if flock -n 200 ; then
+		return 0 
+	else
+		return 1
+	fi
+}
+
+lock || exit 1
+
+# loading credential if exist
+if [ -f $restic_credential_file ] ; then
+	source $restic_credential_file
 fi
 
 clean_exit() {
 	if [ -f $tmp_file ] ; then
 		unlink $tmp_file
-	elif [ -f $lock_file ] ; then
-		rm -f $lock_file 
+	fi
+	if [ -f $lock_file ] ; then
+		rm -f $lock_file
 	fi
 }
+
+logme() {
+	logger
+}
+
+function log_and_exec () {
+	#https://www.linuxquestions.org/questions/linux-newbie-8
+	#/bash-log-and-execute-function-670915/
+       	eval "$@";
+	logger \[$?\] "$@";
+}
+
 
 trap clean_exit 2 3 9
 
